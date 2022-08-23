fff58ccb00fdcd17dc267fc41ffff6a77ef9c574
EmersonLaranja <emersonlaranja@gmail.com>
2022-08-23 15:54:09 -0300

    aula 1 - sem desafios

---
+++ index.js
@@ -0,0 +1,21 @@
+//Assuntos da aula
+var nome = "Emerson";
+
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 7;
+var notaDoTerceiroBimestre = 4;
+var notaDoQuartoBimestre = 2;
+
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre) /
+  4;
+
+var notaFixada = notaFinal.toFixed(1);
+
+console.log("Bem vindo " + nome);
+console.log(notaFixada);
+
+// -------------------------- DESAFIOS -----------------------------
