9b9f517cf6e13e9f46a64fbe236d3497a44c0d35
EmersonLaranja <emersonlaranja@gmail.com>
2022-09-01 15:50:06 -0300

    aula 4.1 - sem desafios [até 50min46s]

--- app.js
+++ app.js
@@ -1,71 +1,22 @@
 //Assuntos da aula
-var numeroSecreto = parseInt(Math.random() * 11);
-console.log(numeroSecreto);
-var numeroDeTentativas = 3;
-var contador = 0;
-function Chutar() {
-  var elementoResultado = document.getElementById("resultado");
+var listaFilmes = ["Yesterday", "A chegada", "Escola de Rock"];
+//elemento              1         2            3
+//indice                0         1            2
 
-  var chute = parseInt(document.getElementById("valor").value);
-  if (chute == numeroSecreto) {
-    elementoResultado.innerHTML = "acertou";
-  } else if (chute > 10 || chute < 0) {
-    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
-  } else {
-    elementoResultado.innerHTML =
-      "Errou, o número secreto era " + numeroSecreto;
-  }
+listaFilmes.push("Harry Potter 2");
+listaFilmes.push("Harry Potter 3");
+listaFilmes.push("Harry Potter 4");
+listaFilmes.push("Harry Potter 5");
+
+// document.write("<p>" + listaFilmes[0] + "</p>");
+// document.write("<p>" + listaFilmes[1] + "</p>");
+// document.write("<p>" + listaFilmes[2] + "</p>");
+// document.write("<p>" + listaFilmes[3] + "</p>");
+
+//   valor inicial         condicao expressao      final
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+  document.write("<p>" + listaFilmes[indice] + "</p>");
 }
+//Até 50min46s
 
 // -------------------------- DESAFIOS -----------------------------
-// 1) Colocar número de tentativas
-// function Chutar() {
-//   var elementoResultado = document.getElementById("resultado");
-//   var elementoTentativa = document.getElementById("tentativa");
-//   var chute = parseInt(document.getElementById("valor").value);
-
-//   contador++;
-
-//   elementoTentativa.innerHTML =
-//     " você tem " + (numeroDeTentativas - contador) + " tentativas";
-
-//   if (chute == numeroSecreto) {
-//     elementoResultado.innerHTML = "Acertou";
-//     contador = 0;
-//     alert("Acertou"); //algo novo, gera um alerta na tela
-//     window.location.reload(); //algo novo, recarrega a página, começando uma nova rodada
-//   } else if (chute > 10 || chute < 0) {
-//     elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
-//   } else {
-//     elementoResultado.innerHTML = "Errado!";
-//   }
-
-//   if (contador == numeroDeTentativas) {
-//     alert("Suas chances acabaram, você perdeu"); //algo novo, gera um alerta na tela
-//     window.location.reload(); //algo novo, recarrega a página, começando uma nova rodada
-//   }
-// }
-// 2) Não mostrar o número secreto quando errar, exibir se é maior/menor (dar uma dica)
-function Chutar() {
-  // var elementoResultado = document.getElementById("resultado");
-  // var chute = parseInt(document.getElementById("valor").value);
-  // if (chute == numeroSecreto) {
-  //   elementoResultado.innerHTML = "acertou";
-  // } else if (chute > numeroSecreto) {
-  //   elementoResultado.innerHTML =
-  //     "Você deve digitar um número menor que " + chute;
-  // } else {
-  //   elementoResultado.innerHTML =
-  //     "Você deve digitar um número maior que " + chute;
-  // }
-}
-// 3) Diferença de == e ===
-/**
-==  é usado para comparação entre duas variáveis, independentemente do tipo de dados da variável. 
-
-=== verifica se seus dois operandos são iguais, retornando um resultado booleano. 
-Ao contrário do operador de igualdade , o operador de igualdade estrita sempre considera operandos 
-de tipos diferentes como diferentes  
-
-fonte: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Strict_equality
-*/
